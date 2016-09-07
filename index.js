f3f7cc3d3198ba021fa1bd5c3798cce71eb64929
Felix Zapata <fzberlinches@gmail.com>
2016-09-07 16:34:42 +0200

    fix(index.js): change how to show the url to test

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ module.exports = function (customOptions, done) {
 		threshold: 0
 	};
 
-	var urlToGet = '';
 	var options = customOptions ? Object.assign(defaultOptions, customOptions) : defaultOptions;
 	var driver = options.browser === 'phantomjs' ? new WebDriver.Builder().withCapabilities(customPhantom).build() : new WebDriver.Builder().forBrowser(options.browser).build();
 	var tagsAreDefined = (!Array.isArray(options.tags) && options.tags !== null && options.tags !== '') || (Array.isArray(options.tags) && options.tags.length > 0);
@@ -36,6 +35,10 @@ module.exports = function (customOptions, done) {
 	var flatten = function(arr) {
 		return [].concat.apply([], arr);
 	};
+
+	var getUrl = function(url) {
+		return isRemoteUrl(url) ? url : fileUrl(url);
+	}
 
 	var createResults = function(results) {
 
@@ -59,11 +62,11 @@ module.exports = function (customOptions, done) {
 
 	var urls = flatten(findGlobPatterns(options.urls));
 
-	Promise.all(urls.map(function(url) {
-			urlToGet = isRemoteUrl(url) ? url : fileUrl(url); 
+
+	Promise.all(urls.map(function(url) { 
 			return new Promise(function(resolve) {
 				driver
-					.get(urlToGet)
+					.get(getUrl(url))
 					.then(function() {
 						var startTimestamp = new Date().getTime();
 						var axeBuilder = new AxeBuilder(driver);
@@ -73,7 +76,7 @@ module.exports = function (customOptions, done) {
 						}
 
 						axeBuilder.analyze(function(results) {
-							results.url = urlToGet;
+							results.url = url;
 							results.timestamp = new Date().getTime();
 							results.time = results.timestamp - startTimestamp;
 							resolve(results);
